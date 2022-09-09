5a111535f141c81b1fc39847f85abe88224d99fe
Prakhar Chakravarty <80689838+prakhar-198@users.noreply.github.com>
2022-09-09 22:57:02 +0530

    using stack

---
+++ maxhistarea.java
@@ -0,0 +1,65 @@
+
+import java.io.*;
+
+import java.util.*;
+
+public class maxhistarea {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(br.readLine());
+        }
+
+
+        int[] rb = new int[n];
+        Stack<Integer> st = new Stack<>();
+        st.push(n - 1);
+        rb[n - 1] = n;
+
+        for (int i = n - 2; i >= 0; i--) {
+            while (st.size() > 0 && a[i] <= a[st.peek()]) {
+                st.pop();
+            }
+            if (st.size() > 0) {
+                rb[i] = st.peek();
+            } else {
+                rb[i] = n;
+            }
+            st.push(i);
+        }
+
+        int[] lb = new int[n];
+
+        st = new Stack<>();
+        st.push(0);
+        lb[0] = -1;
+
+        for (int i = 1; i < n; i++) {
+            while (st.size() > 0 && a[i] <= a[st.peek()]) {
+                st.pop();
+            }
+            if (st.size() > 0) {
+                lb[i] = st.peek();
+            } else {
+                lb[i] = -1;
+            }
+            st.push(i);
+        }
+
+        int maxArea = 0;
+        for (int i = 0; i < n; i++) {
+            int width = rb[i] - lb[i] - 1;
+            int area = a[i] * width;
+
+            if (area > maxArea) {
+                maxArea = area;
+            }
+        }
+
+        System.out.println(maxArea);
+    }
+}
