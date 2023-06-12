bdf478502df5ba1aa4b6513e8d244670e751ac3b
Prakhar Chakravarty <80689838+prakhar-198@users.noreply.github.com>
2023-06-12 22:33:51 +0530

    leetcode128

---
+++ longestConsecutive.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        // if array is empty, return 0
+        if (nums.length == 0) 
+        {
+            return 0;
+        }
+        Set<Integer> set = new HashSet<Integer>();
+        int max = 1;
+        for (int e : nums)
+            set.add(e);
+        for (int e : nums) 
+        {
+            int left = e - 1;
+            int right = e + 1;
+            int count = 1;
+            while (set.contains(left)) 
+            {
+                count++;
+                set.remove(left);
+                left--;
+            }
+            while (set.contains(right)) 
+            {
+                count++;
+                set.remove(right);
+                right++;
+            }
+            max = Math.max(count, max);
+        }
+        return max;
+    }
+}
